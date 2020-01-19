2abcb4f5ae5595bc4273f54e08f833620b445115
Greg Hughes <com.greghughes.MemeKeyboard>
2020-01-18 19:05:50 -0700

    first commit

---
+++ Code wars 1.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var str = "my name is mr. poo poo head"
+extension String {
+    func toJadenCase() -> String {
+    var convertedWords = ""
+    var pocket: [String] = [""]
+    
+    
+    pocket = self.components(separatedBy: " ")
+        for (index,i) in pocket.enumerated() {
+        var word = ""
+            
+        if index != pocket.count - 1 {
+            word = i.prefix(1).uppercased() + i.dropFirst() + " "
+        } else {
+            word = i.prefix(1).uppercased() + i.dropFirst()
+        }
+        
+        convertedWords += word
+    }
+    return convertedWords
+  }
+}
+
+"How can mirrors be real if our eyes aren't real".toJadenCase()
