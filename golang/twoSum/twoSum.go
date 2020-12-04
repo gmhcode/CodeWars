4ab10da1a0734700900987d34608154da0df4167
gmhcode <greg.hughes@meritagepos.com>
2020-12-04 14:26:27 -0700

    twoSum golang complete

---
+++ golang/twoSum/twoSum.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+// Based on: http://oj.leetcode.com/problems/two-sum/
+
+// twoSum [1, 2, 3] 4 === (0, 2)
+
+func main() {
+	fmt.Println("Answer: ", twoSum([]int{1, 2, 3}, 4))
+}
+
+func twoSum(numbers []int, target int) [2]int {
+	numberCount := len(numbers)
+
+	for numInd, num := range numbers {
+		for kInd, k := range numbers[numInd+1 : numberCount] {
+			if num+k == target {
+
+				return [2]int{numInd, kInd + numInd + 1}
+			}
+		}
+	}
+
+	return [2]int{}
+}
+
+// Expect(TwoSum([]int{1, 2, 3}, 4)).To(Equal([2]int{0, 2}))
+
+// Expect(TwoSum([]int{1234, 5678, 9012}, 14690)).To(Equal([2]int{1, 2}))
+
+// Expect(TwoSum([]int{2, 2, 3}, 4)).To(Equal([2]int{0, 1}))
